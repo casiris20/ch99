e5b87329ba2885db16060361bdf19d4eb7632592
casiris20 <47557740+casiris20@users.noreply.github.com>
2020-07-14 17:01:58 +0900

    연습2

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,14 +17,15 @@ from django.contrib import admin
 from django.urls import path
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from bookmark.views import BookmarkLV, BookmarkDV
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('bookmark/', include('bookmark.urls')),
+    path('blog/', include('blog.urls')),
     # class-based views
-    path('bookmark/', BookmarkLV.as_view(), name='index'),
-    path('bookmark/<int:pk>/', BookmarkDV.as_view(), name='detail'),
+    # path('bookmark/', BookmarkLV.as_view(), name='index'),
+    # path('bookmark/<int:pk>/', BookmarkDV.as_view(), name='detail'),
 ]
